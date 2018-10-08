0ca26ce029205f94c773b2caa6628f7e68e827f2
guillermo adrian <gjadriant@gmail.com>
2018-10-08 17:45:26 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -8,17 +8,33 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
+          <p contentEditable>
+            Workflowy clone
           </p>
           <a
             className="App-link"
-            href="https://reactjs.org"
+            href="workflowy.com"
             target="_blank"
             rel="noopener noreferrer"
           >
             Learn React
           </a>
+          <ul>
+            <li>
+            * display/deconstruct json file from io 
+            </li>
+            <li>
+            ** make it editable 
+            </li>
+            <li>
+            * zoom and unzoom
+            </li>
+            <li>
+            * use object as state
+            </li>
+            </ul>
+
+
         </header>
       </div>
     );
---
+++ src/components/Item.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+//TODO
+// + BUTTON THAT MINIMIZES AND UNMINIMIZES THE VALUE, BE IT TEXT OR MORE LIST
+// MINIMIZED ONLY SHOWS KEY
+// ONCLICK
+
+const Item = ({key, value}) => {
+  return (
+    <div>
+    </div>
+  )
+}
+
+export default App;
