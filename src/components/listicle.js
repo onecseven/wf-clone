7f6a0d2c05e2355d6872540e47914806f6f59357
guillermo adrian <gjadriant@gmail.com>
2018-10-13 14:36:50 -0400

    listicle

--- src/components/listicle.js
+++ src/components/listicle.js
@@ -2,6 +2,35 @@ Array.prototype.last = function () {
   return this[this.length-1]
 }
 
+var checkArrays = function (arr1, arr2){
+  var nested = function(item){
+    if (Array.isArray(item)){
+      return true;
+    }
+    return false;
+  }
+  if (!Array.isArray(arr1) || !Array.isArray(arr2)){
+    return false;
+  }
+  if (arr1.length !== arr2.length){
+    return false;
+  }
+  if (arr1.every(nested) && arr2.every(nested)){ //nested case
+    for (var q = 0; q < arr1.length; q++){
+      if(!checkArrays(arr1[q], arr2[q])){ //recursion at one level of recursion
+        return false;
+      }
+    }
+  } else {  //not nested case
+      for (var i = 0; i < arr1.length; i++){
+        if (arr1[i] !== arr2[i]){
+            return false;
+          }
+      }
+  }
+  return true;
+}
+
 export default class listitem {
   constructor(text, parent=null, children=[]){
     this.value = text
@@ -10,13 +39,40 @@ export default class listitem {
   }
   addChild(child, subchildren=[]){
     this.children.push(new listitem(child, this, subchildren))
-    return this.children[this.children.length - 1]
+    return this.children.last()
     }
+  equals(item){
+    if (item.value !== this.value){
+      return false
+    } else if (!checkArrays(item.children, this.children)){
+      return false
+    } else {
+      return true
+    }
+  }
 }
 
 class list{
   constructor(){
     this.store = []
+  }
+  originArray(item){
+  //returns the array in which it is, and its index
+  let det = (item, store) => {
+    for (let i = 0; i < store.length; i++) {
+      const el = store[i]
+      if (el.equals(item)){
+        return [store, i]
+        }
+      if (el.children.length > 0){
+        if (det(item, el.children)){
+          return det(item, el.children)
+          } 
+        }
+      }
+    return false;
+    }
+    return det(item, this.store)
   }
   enter(text, parent=null){
     //returns what you enter so you can save it
@@ -27,7 +83,16 @@ class list{
       return parent.addChild(text)
     }
   }
-  tab(){
+  tab(item){
+    if (this.originArray(item)){
+      var origin = this.originArray(item)[0]
+      var index = this.originArray(item)[1]
+    } else {
+      throw new Error('problem tabbing')
+    }
+    if (index > 0){
+      origin[index-1].addChild(item.value, item.children)
+    }
   }
   untab(item){
     let temp = item.parent.parent.addChild(item.value, item.children)
@@ -35,4 +100,28 @@ class list{
   }
   delete(){
   }
+  display(){
+    let turn = (item) => {
+      var temp = []      
+      if (item.children && item.children.length > 0){
+        item.children.forEach(el => {
+          temp.push(turn(el))
+        });
+      }
+      return [item.value, temp]
+    }
+    return JSON.stringify(this.store.map(el => {
+      return turn(el)
+    }),null, 2)
+  }
 }
+
+let ls = new list()
+let one = ls.enter('hi')
+let two = ls.enter('sarah', one)
+let twoo = ls.enter('sarah2', one)
+let three = ls.enter('natalia')
+let four = ls.enter('star', three)
+ls.display() //?
+
+//tab -> move to .parent.children[i-1].children
